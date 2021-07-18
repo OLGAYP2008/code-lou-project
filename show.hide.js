ff91d0e427e6181615fb16e226783601143055e2
OLGAYP2008 <olgayp2008@gmail.com>
2021-07-18 19:13:31 -0400

    many formattimg changes

---
+++ show.hide.js
@@ -0,0 +1,14 @@
+$('#show').click(function()
+{
+$('#show').css('display,"none');
+$('#data').show();
+$('#hide').show()
+});
+
+$('#hide').click(function()
+{
+$('#hide').css('display,"none');
+$('#data').hide();
+$('#hide').hide()
+});
+
