a6464a5bd411f6b57c84dc7d2abcef84a4270524
OLGAYP2008 <olgayp2008@gmail.com>
2021-07-21 22:59:56 -0400

    parameters for boxes

---
+++ order/show.hide.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("Show Text");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
